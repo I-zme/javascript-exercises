974f126e0533f84cee81e8cc8c508e9ad40d5137
I-zme <inbar.amir21@gmail.com>
2022-09-05 16:32:21 +0700

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(a_string) {
+    let reversedString='';
+    for(let i=a_string.length;i>0;i--){
+        reversedString += a_string[i-1];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
