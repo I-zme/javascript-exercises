cfabca2f2eb532d36fc01492febeb364b1b204e8
I-zme <inbar.amir21@gmail.com>
2022-09-06 18:57:10 +0700

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fdeg) {
+  celsiusDeg = (fdeg-32)*(5/9);
+  celsiusDeg = Number(celsiusDeg.toFixed(1));
+  return celsiusDeg
 };
 
-const ctof = function() {
-
+const ctof = function(cdeg) {
+  fahrenDeg = cdeg*(9/5) + 32;
+  fahrenDeg = Number(fahrenDeg.toFixed(1));
+  return fahrenDeg
 };
 
 // Do not edit below this line
