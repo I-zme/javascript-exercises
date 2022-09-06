0c245c3ba1ce2dd348e88dde95d6adf5b102cfb7
I-zme <inbar.amir21@gmail.com>
2022-09-06 13:39:49 +0700

    Complete 04 --CHECK SOLUTION LATER

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...Args) {
 
+    OuterI:for(let i=0; i<=myArray.length;i++){
+        InnerJ:for(let j=0; j<Args.length;j++){
+            console.log(Args[j]);//debugging
+
+            if(myArray[i]===Args[j]){
+                myArray.splice(i,1);
+
+                console.log(myArray+"=myArray");
+                if(i===0){i=-1;}
+                else{i--;}
+                break InnerJ;
+            }
+        }
+    }
+    return myArray
 };
 
 // Do not edit below this line
