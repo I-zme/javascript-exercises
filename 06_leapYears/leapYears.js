b259646eab74f0514a21ade1abf4946eda0cfced
I-zme <inbar.amir21@gmail.com>
2022-09-06 17:07:25 +0700

    Complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+/*
+if (year/4 )
+    if year/100 && year/400) || !year/100
+    return leap year
+*/
 
+const leapYears = function(userYear) {
+    let isleapyear = false;
+    if(userYear%4===0){
+        if(!(userYear%100===0) || userYear%400===0){
+            isleapyear = true;
+        }
+    }
+    
+    return isleapyear
 };
 
 // Do not edit below this line
