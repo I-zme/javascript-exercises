cc08dda6b83a883c040188a7567c269379670297
I-zme <inbar.amir21@gmail.com>
2022-09-07 11:44:36 +0700

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    let newString=[];
+    let Palindrome="ERROR";
+    for(char of myString.toLowerCase()){
+        if(char.match(/[a-z]/)){
+            newString.push(char);
+        }
+    }
 
+    isPalindrome:for(let i=0;i<=Math.floor(newString.length/2);i++){
+        while(newString[i]!==newString[newString.length-1-i]){
+            Palindrome=false;
+            break isPalindrome
+        }
+    Palindrome = true;
+    }
+    return Palindrome
 };
 
 // Do not edit below this line
