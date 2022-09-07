81b76c4de6fa5b8071729aed3198c4fbb79ea27a
I-zme <inbar.amir21@gmail.com>
2022-09-07 16:49:47 +0700

    Complete 10 --CHECK SOLUTION LATER

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if(member<0){
+        answer= "OOPS";
+    }
+    else{
+        member=parseInt(member);
+        let fibonacciSequence = [0,1];
+        if(member>=fibonacciSequence.length){
+            for(let i=2; i<=member;i++){
+                let nextSequenceNumber = fibonacciSequence[i-2]+fibonacciSequence[i-1];
+                fibonacciSequence.push(nextSequenceNumber);
+            }
+        }
+        answer= fibonacciSequence[member];
+    }
+    return answer
 };
 
 // Do not edit below this line
